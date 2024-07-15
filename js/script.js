4daba7a0ca077ae42b6b981abed88e604fcfa6b3
yous0001 <mad33833@gmail.com>
2024-07-15 20:20:09 +0300

    read operation

--- js/script.js
+++ js/script.js
@@ -12,4 +12,28 @@ function createProduct(){
         prodDescrition:productDescrition.value
     }
     products.push(product)
+    reset()
+    display()
+}
+function reset(){
+    productName.value=""
+    productCategory.value=""
+    productPrice.value=""
+    productDescrition.value=""
+}
+
+function display(){
+    let cartona="";
+    for(let i=0;i<products.length;i++){
+        cartona+=`<tr>
+                    <td>${i+1}</td>
+                    <td>${products[i].prodName}</td>
+                    <td>${products[i].prodCategory}</td>
+                    <td>${products[i].prodPrice}</td>
+                    <td>${products[i].prodDescrition}</td>
+                    <td><button class="btn btn-outline-warning"><i class="fa-solid fa-pen-to-square"></i></button></td>
+                    <td><button class="btn btn-outline-danger"><i class="fa-solid fa-trash"></i></button></td>
+                </tr>`
+    }
+    document.getElementById("tableBody").innerHTML=cartona;
 }
